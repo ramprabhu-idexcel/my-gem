ed9cfe2f3bd460400695a2738311764e816468b7
ramprabhu-idexcel <nramprabu@gmail.com>
2016-12-22 18:19:26 +0530

    validate rating range

--- lib/mygem/review.rb
+++ lib/mygem/review.rb
@@ -1,6 +1,6 @@
 module Mygem
   class Review < ActiveRecord::Base
     belongs_to :restaurant
-    validates :rating, :inclusion => 1..5
+    validates :rating, :inclusion => 1..5, message: 'rating must be between 1..5'
   end
 end
