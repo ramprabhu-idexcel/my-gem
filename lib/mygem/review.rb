7b079f9010e21171543d3736ea28f98fd638b3bd
ramprabhu-idexcel <nramprabu@gmail.com>
2016-12-26 13:12:29 +0530

    review user association

--- lib/mygem/review.rb
+++ lib/mygem/review.rb
@@ -1,6 +1,7 @@
 module Mygem
   class Review < ActiveRecord::Base
     belongs_to :restaurant
+    belongs_to :user
     validates :rating, inclusion: { in: 1..5, message: 'rating must be between 1..5' }
   end
 end
