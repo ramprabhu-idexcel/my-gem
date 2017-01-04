326ebeb8e395c6cc69a4a7e2a9e177e05c2da59a
ramprabhu-idexcel <nramprabu@gmail.com>
2017-01-04 14:49:21 +0530

    Gk, computer model changes

--- lib/mygem/computer.rb
+++ lib/mygem/computer.rb
@@ -1,7 +1,9 @@
 module Mygem
   class Computer < Quiz
+=begin
     def self.model_name
       Quiz.model_name
     end
+=end
   end
 end
--- lib/mygem/gk.rb
+++ lib/mygem/gk.rb
@@ -1,7 +1,5 @@
 module Mygem
   class Gk < Quiz
-    def self.model_name
-      Quiz.model_name
-    end
+
   end
 end
