761ab7874be8eb91c3d4486e988d6eeb1b42c180
ramprabhu-idexcel <nramprabu@gmail.com>
2017-01-05 13:06:35 +0530

    searalize quiz

--- lib/mygem/quiz.rb
+++ lib/mygem/quiz.rb
@@ -1,6 +1,6 @@
 module Mygem
   class Quiz < ActiveRecord::Base
-    #serialize :options, Array
+    serialize :options, Array if ActiveRecord::Base.connection.adapter_name == "Mysql2"
     validates :question, :answer, :type, :options, presence: true
   end
 end
