e910cf5d7dd578252ce719dd292e62b68ab035ac
ramprabhu-idexcel <nramprabu@gmail.com>
2017-01-16 14:47:25 +0530

    Restaurant db details

--- lib/mygem/restaurant.rb
+++ lib/mygem/restaurant.rb
@@ -1,5 +1,26 @@
 module Mygem
   class Restaurant < ActiveRecord::Base
     has_many :reviews, dependent: :destroy
+
+    class << self
+      def best
+        Restaurant
+            .joins(:reviews)
+            .select("restaurants.*, AVG(reviews.rating) as avg_rating")
+            .group("restaurants.id")
+            .having("avg_rating > 3")
+            .limit(10)
+      end
+
+      def great_reviews
+        Restaurant
+            .joins(:reviews)
+            .select("restaurants.*, COUNT(reviews.id) as reviews_count")
+            .group("restaurants.id")
+            .where("reviews.created_at > ?", 3.months.ago)
+            .having("COUNT(reviews.id) > 10")
+            .limit(10)
+      end
+    end
   end
 end
